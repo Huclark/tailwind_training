44f5b9d8fc8d97b2c061d028574002e0b02fcd6a
Huclark <vhuclark@gmail.com>
2024-05-22 13:15:51 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#0fa9e6",
+          light: "#3fbaeb",
+          dark: "#0c87b8",
+        }
+      },
+    },
   },
   plugins: [],
 }
